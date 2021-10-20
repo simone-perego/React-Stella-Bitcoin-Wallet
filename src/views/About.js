81b7573aa2270af9bed1b2728c0921380dd5a0ce
simone-perego <simo_bryant@hotmail.it>
2021-10-20 22:36:14 +0200

    minor css fixes

--- src/views/About.js
+++ src/views/About.js
@@ -9,17 +9,12 @@ function About() {
       <div className="about">
         <p>
           Lorem Ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
           consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
           est laborum.
         </p>
-        <img
-          src="https://st2.depositphotos.com/3591429/10566/i/600/depositphotos_105666254-stock-photo-business-people-at-meeting-and.jpg"
-          alt="Trulli"
-          width="500"
-          height="333"
-        ></img>
+        <img src="https://st2.depositphotos.com/3591429/10566/i/600/depositphotos_105666254-stock-photo-business-people-at-meeting-and.jpg"></img>
       </div>
     </div>
   );
